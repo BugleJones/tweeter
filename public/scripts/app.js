f7de18870544f1924509bd32f8801789decb9dd9
BugleJones <spencerjhamilton@gmail.com>
2017-09-20 03:22:10 +0000

    Continuing to work on tweet functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,59 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+var $;
+
+var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+$(function() {
+  function createTweetElement(tweetObject) {
+    const $tweet = $("<article>").addClass("tweet");
+    $(".all-tweets").append($tweet);
+
+    const img = $("<img>").attr("src", tweetObject.user.avatars.small);
+    const user = $("<p>").text(tweetObject.user.name);
+    const handle = $("<p>").text(tweetObject.user.handle);
+
+    const $header = $("<header>")
+      .append(img)
+      .append(user)
+      .append(handle);
+
+    $tweet.append($header);
+
+    const $tweetContent = $("<div>").addClass("tweet-content");
+    $(".tweet-content").append($tweetContent);
+
+    const content = $("<p>").text(tweetObject.content.text);
+
+    const $content = $("<p>")
+      .append(content);
+
+    $tweet.append($content);
+
+    
+
+
+
+
+
+      // .append($content);
+      // .append($footer);
+
+      $(".all-tweets").append($tweet);
+  }
+
+  createTweetElement(tweetData);
+
+});
