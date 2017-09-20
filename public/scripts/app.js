c24f37ae02852a8805e7e6de0e1ad51f46076796
BugleJones <spencerjhamilton@gmail.com>
2017-09-20 18:23:24 +0000

    Added skeleton of time-ago function to be refactored later

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -47,8 +47,44 @@ var tweetData = [
   }
 ];
 
-function createTweetElement(tweetObject) {
-  $(function() {
+$(function() {
+
+  function renderTweets(tweetsArray) {
+    tweetsArray.forEach(tweetData => {
+      createTweetElement(tweetData);
+    });
+    return tweetsArray;
+  }
+
+  renderTweets(tweetData);
+
+  // function timeSince(date) {
+  //   var seconds = Math.floor((new Date() - date) / 1000);
+  //   var interval = Math.floor(seconds / 31536000);
+  //   if (interval > 1) {
+  //     return interval + " years";
+  //   }
+  //   interval = Math.floor(seconds / 2592000);
+  //   if (interval > 1) {
+  //     return interval + " months";
+  //   }
+  //   interval = Math.floor(seconds / 86400);
+  //   if (interval > 1) {
+  //     return interval + " days";
+  //   }
+  //   interval = Math.floor(seconds / 3600);
+  //   if (interval > 1) {
+  //     return interval + " hours";
+  //   }
+  //   interval = Math.floor(seconds / 60);
+  //   if (interval > 1) {
+  //     return interval + " minutes";
+  //   }
+  //   return Math.floor(seconds) + " seconds";
+  // }
+  //   var aDay = 24*60*60*1000;
+
+  function createTweetElement(tweetObject) {
     const $tweet = $("<article>").addClass("tweet");
     $(".all-tweets").append($tweet);
 
@@ -84,15 +120,5 @@ function createTweetElement(tweetObject) {
       .append($footerIcons);
 
     $tweet.append($footer);
-
-  });
-}
-
-function renderTweets(tweetsArray) {
-  tweetsArray.forEach(tweetData => {
-    createTweetElement(tweetData);
-  });
-  return tweetsArray;
-}
-
-renderTweets(tweetData);
+  }
+});
