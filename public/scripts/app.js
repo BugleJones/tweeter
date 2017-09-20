dc3eb604be722eb59b2a6c5493288ec957fee717
BugleJones <spencerjhamilton@gmail.com>
2017-09-20 04:06:11 +0000

    Reconfigured app.js to inherit CSS styling with more specfic pathways

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,44 +14,41 @@ var tweetData = {
     "text": "If I have seen further it is by standing on the shoulders of giants"
   },
   "created_at": 1461116232227
-}
+};
 
 $(function() {
   function createTweetElement(tweetObject) {
     const $tweet = $("<article>").addClass("tweet");
-    $(".all-tweets").append($tweet);
 
     const img = $("<img>").attr("src", tweetObject.user.avatars.small);
     const user = $("<p>").text(tweetObject.user.name);
     const handle = $("<p>").text(tweetObject.user.handle);
 
-    const $header = $("<header>")
-      .append(img)
-      .append(user)
-      .append(handle);
+    const $header = $("<header>").append(img, user, handle);
 
     $tweet.append($header);
 
-    const $tweetContent = $("<div>").addClass("tweet-content");
-    $(".tweet-content").append($tweetContent);
-
-    const content = $("<p>").text(tweetObject.content.text);
-
-    const $content = $("<p>")
+    const content = $("<p>").addClass("tweet-detail").text(tweetObject.content.text);
+    const $content = $("<div>").addClass("tweet-content")
       .append(content);
 
     $tweet.append($content);
 
-    
+    const createdAt = $("<p>").text(Date(tweetObject.user.created_at))
+    const flagIcon = $("<i>").addClass("fa fa-exclamation-triangle");
+    const retweetIcon = $("<i>").addClass("fa fa-retweet");
+    const favouriteIcon = $("<i>").addClass("fa fa-star");
 
+    const $footerTime = $("<div>").addClass("time-ago").append(createdAt);
+    const $footerIcons = $("<div>").addClass("icons").append(flagIcon, retweetIcon, favouriteIcon);
 
+    const $footer = $("<footer>")
+      .append($footerTime)
+      .append($footerIcons)
 
+    $tweet.append($footer);
 
-
-      // .append($content);
-      // .append($footer);
-
-      $(".all-tweets").append($tweet);
+    $(".all-tweets").append($tweet);
   }
 
   createTweetElement(tweetData);
